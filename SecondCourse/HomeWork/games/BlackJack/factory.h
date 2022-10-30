ac7503b294bb761e778287be4ffd76703420dfd2
Daniil Lanin <d.lanin@g.nsu.ru>
2022-10-30 20:29:38 +0700

    edit factory

--- SecondCourse/HomeWork/games/BlackJack/factory.h
+++ SecondCourse/HomeWork/games/BlackJack/factory.h
@@ -6,12 +6,31 @@
 #include "strategies.h"
 
 template <typename key, typename value>
-class Factory {
+class Factory
+{
 public:
-    Factory();
-    Player* CreateBot(const key&);
-    void RegisterStrategy(const key&, value * (*)());
-protected:
+    Factory()
+    {
+        RegisterStrategy("-trivialBot1", CreateTrivialBot1);
+        RegisterStrategy("-trivialbot1", CreateTrivialBot1);
+        RegisterStrategy("-trivialBot2", CreateTrivialBot2);
+        RegisterStrategy("-trivialbot2", CreateTrivialBot2);
+        RegisterStrategy("-trivialBot3", CreateTrivialBot3);
+        RegisterStrategy("-trivialbot3", CreateTrivialBot3);
+        RegisterStrategy("-bot1", CreateBot1);
+        RegisterStrategy("-bot2", CreateBot2);
+        RegisterStrategy("-metabot", CreateMetaBot);
+    }
+    Player* CreateBot(const key& name)
+    {
+        if (Strategies_.count(name) == 0) return nullptr;
+        return Strategies_[name]();
+    }
+    void RegisterStrategy(const key& name, value * (*creator)())
+    {
+        Strategies_[name] = creator;
+    }
+private:
     std::map <key, value * (*)()> Strategies_;
 };
 
