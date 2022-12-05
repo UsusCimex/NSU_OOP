7772bba073d0f1227b9a2b15803d231d312e2538
Daniil Lanin <d.lanin@g.nsu.ru>
2022-12-05 23:32:49 +0700

    Start logic

--- HomeWork/games/TetrisQT/Tetris/tetris.cpp
+++ HomeWork/games/TetrisQT/Tetris/tetris.cpp
@@ -1,12 +1,72 @@
 #include "tetris.h"
 
+#include <QDebug>
+#include <QPainter>
+
 Tetris::Tetris(QWidget *parent)
     : QWidget(parent)
 {
+    this->setWindowTitle("Tetris game");
 
+    this->resize(DOT_WIDTH * FIELD_WIDTH, DOT_HEIGHT * FIELD_HEIGHT);
+    this->setFixedSize(DOT_WIDTH * FIELD_WIDTH, DOT_HEIGHT * FIELD_HEIGHT);
+
+    initGame();
 }
 
 Tetris::~Tetris()
 {
 
+}
+
+void Tetris::timerEvent(QTimerEvent * event)
+{
+//    qDebug() << 1;
+}
+
+void Tetris::keyPressEvent(QKeyEvent * event)
+{
+    int key = event->key();
+    switch (key)
+    {
+        case Qt::Key_Left:
+            qDebug() << "A";
+            break;
+        case Qt::Key_Right:
+            qDebug() << "D";
+            break;
+    }
+}
+
+void Tetris::paintEvent(QPaintEvent * event)
+{
+    Q_UNUSED(event);
+    doDrawing();
+}
+
+void Tetris::initGame() {
+    _inGame = true;
+    _delay = 150;
+    timerID = startTimer(_delay);
+
+    createDetail();
+}
+
+void Tetris::createDetail()
+{
+
+}
+
+void Tetris::doDrawing()
+{
+    QPainter qp(this);
+
+    if (_inGame)
+    {
+
+    }
+    else
+    {
+//        gameOver();
+    }
 }
--- HomeWork/games/TetrisQT/Tetris/tetris.h
+++ HomeWork/games/TetrisQT/Tetris/tetris.h
@@ -2,6 +2,8 @@
 #define TETRIS_H
 
 #include <QWidget>
+#include <QPoint>
+#include <QKeyEvent>
 
 class Tetris : public QWidget
 {
@@ -11,29 +13,48 @@ public:
     Tetris(QWidget *parent = nullptr);
     ~Tetris();
 
+protected:
+    void timerEvent(QTimerEvent *) override;
+    void keyPressEvent(QKeyEvent *) override;
+    void paintEvent(QPaintEvent *) override;
+
 private:
-    static constexpr int M = 20;
-    static constexpr int N = 10;
-    int field[M][N] = {0};
-    int figures[7][4] =
-    {
-        1,3,5,7, //I
-        2,4,5,7, //Z
-        3,5,4,6, // S
-        3,5,4,7, // T
-        2,3,5,7, // L
-        3,5,7,6, // J
-        2,3,4,5, // O
-    };
+    static constexpr int DOT_WIDTH = 20;
+    static constexpr int DOT_HEIGHT = 20;
 
-    bool check();
-    void draw();
-    void checkLines();
-    void rotate();
-    void move();
+    static constexpr int FIELD_WIDTH = 15;
+    static constexpr int FIELD_HEIGHT = 30;
 
-protected slots:
+    int _delay;
+    int timerID;
 
+    bool _inGame;
+    int* field[FIELD_WIDTH][FIELD_HEIGHT] = {0};
+
+    QPoint detail[4];
+    QPoint movedDetail[4];
+
+    void initGame();
+    void createDetail();
+    void doDrawing();
+
+//    int field[M][N] = {0};
+//    int figures[7][4] =
+//    {
+//        1,3,5,7, //I
+//        2,4,5,7, //Z
+//        3,5,4,6, // S
+//        3,5,4,7, // T
+//        2,3,5,7, // L
+//        3,5,7,6, // J
+//        2,3,4,5, // O
+//    };
+
+//    bool check();
+//    void draw();
+//    void checkLines();
+//    void rotate();
+//    void move();
 };
 
 #endif // TETRIS_H
