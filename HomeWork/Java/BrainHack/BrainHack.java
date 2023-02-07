a07b21fe65bf9f2be075e241d9d7c00d09f50003
UsusCimex <d.lanin@g.nsu.ru>
2023-02-08 00:48:11 +0700

    change main file

--- HomeWork/Java/BrainHack/BrainHack.java
+++ HomeWork/Java/BrainHack/BrainHack.java
@@ -1,7 +1,19 @@
 import operation.*;
+import data.*;
+import logic.*;
 
 public class BrainHack {
     public static void main(String[] args) {
-        
+        if (args.length != 1) { System.out.println("Enter PATH to executable file!"); }
+        else execute(args[0]);
+    }
+    public static void execute(String path) {
+        int pointer = 0;
+        CommandBuffer buffer = new CommandBuffer(path);
+        while (pointer < buffer.getFileSize() && pointer > 0) {
+            char cmd = buffer.getCommand(pointer);
+            Operation op = OperationFactory.create(cmd);
+            pointer += op.make();
+        }
     }
 }
