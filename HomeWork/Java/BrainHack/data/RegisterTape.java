46f76b0424caf4c4a8960bfbb1ae35604eea35db
UsusCimex <d.lanin@g.nsu.ru>
2023-02-08 17:47:56 +0700

    change registertape logic

--- HomeWork/Java/BrainHack/data/RegisterTape.java
+++ HomeWork/Java/BrainHack/data/RegisterTape.java
@@ -1,20 +1,28 @@
 package data;
 
+import java.util.Arrays;
+
 public abstract class RegisterTape {
     private static final int TAPE_SIZE = 30000; // 30000 default value by U.Muller(author)
 
     public static char getCellValue() {
         return arr[index];
     }
-    public static void nextCell() {
-        if (index >= TAPE_SIZE) {} //throw
-        index++;
+    public static void setCellValue(char value) {
+        arr[index] = value;
     }
-    public static void backCell() {
-        if (index <= 0) {} //throw
-        index--;
+
+    public static Integer getCellIndex() {
+        return index;
+    }
+    public static void setCellIndex(int value) {
+        if (value < 0 || value >= TAPE_SIZE) {} //throw
+        index = value;
     }
 
     private static char[] arr = new char[TAPE_SIZE];
     private static Integer index = 0;
+    static {
+        Arrays.fill(arr, (char)0);
+    }
 }
