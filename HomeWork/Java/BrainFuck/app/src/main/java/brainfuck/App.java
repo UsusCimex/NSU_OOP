8730cb46f4b1822facee5015304db6bff458c09b
UsusCimex <d.lanin@g.nsu.ru>
2023-02-21 01:59:17 +0700

    Fix space checks

--- HomeWork/Java/BrainFuck/app/src/main/java/brainfuck/App.java
+++ HomeWork/Java/BrainFuck/app/src/main/java/brainfuck/App.java
@@ -9,10 +9,10 @@ import brainfuck.logic.*;
 
 public class App {
     public static void main(String[] args) {
-        if (args.length != 1) {
+        if (args.length != 0) {
             System.out.println("Enter PATH to executable file!");
         } else {
-            execute(args[1]); //"/home/danil/git/21212_lanin/HomeWork/Java/BrainFuck/app/src/main/resources/code.bf"
+            execute("/home/danil/git/21212_lanin/HomeWork/Java/BrainFuck/app/src/main/resources/code.bf"); //args[1]
         }
     }
 
@@ -21,7 +21,7 @@ public class App {
             CommandContext context = new CommandContext(path);
             while (context.pointer < context.commandBuffer.getFileSize() && context.pointer >= 0) {
                 String cmd = Character.toString(context.commandBuffer.getCommand(context.pointer));
-                if (cmd == "\n" || cmd == " ") {
+                if (cmd.charAt(0) == ' ' || cmd.charAt(0) == '\n') {
                     context.pointer++;
                     continue;
                 }
@@ -30,11 +30,11 @@ public class App {
             }
         }
         catch(FileNotFoundException ex) {
-            System.err.print("File not found ^_^");
+            System.err.println("File not found ^_^");
         }
         catch(Exception ex) {
-            System.err.print("BF destroyd...(" + ex.getMessage() + ")");
+            System.err.println("BF destroyd...");
+            ex.printStackTrace();
         }
-        System.err.print("\n");
     }
 }
