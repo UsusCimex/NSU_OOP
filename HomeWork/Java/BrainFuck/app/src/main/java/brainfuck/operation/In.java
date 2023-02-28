cc00b023c200a8482807727ec0a438e64a570bf8
UsusCimex <d.lanin@g.nsu.ru>
2023-03-01 01:27:31 +0700

    Update input and output logger info

--- HomeWork/Java/BrainFuck/app/src/main/java/brainfuck/operation/In.java
+++ HomeWork/Java/BrainFuck/app/src/main/java/brainfuck/operation/In.java
@@ -2,15 +2,20 @@ package brainfuck.operation;
 
 import java.util.Scanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import brainfuck.data.CommandContext;
 
 //** Считывание данных с консоли. */
 public class In implements Operation {
+    private static final Logger logger = LogManager.getLogger(In.class);
     private static Scanner scan = new Scanner(System.in);
     @Override
     public void run(CommandContext context) {
         Integer read = scan.nextInt();
         context.registerTape.setCellValue(read);
         context.pointer++;
+        logger.info("Readed: " +  read);
     }
 }
--- HomeWork/Java/BrainFuck/app/src/main/java/brainfuck/operation/Out.java
+++ HomeWork/Java/BrainFuck/app/src/main/java/brainfuck/operation/Out.java
@@ -2,11 +2,17 @@ package brainfuck.operation;
 
 import brainfuck.data.CommandContext;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 //** Вывод содержимого ячейки. */
 public class Out implements Operation {
+    private static final Logger logger = LogManager.getLogger(Out.class);
     @Override
     public void run(CommandContext context) {
-        System.out.print(context.registerTape.getCellValue() + " ");
+        int val = context.registerTape.getCellValue();
+        System.out.print(val + " ");
         context.pointer++;
+        logger.info("Writed: " +  val);
     }
 }
