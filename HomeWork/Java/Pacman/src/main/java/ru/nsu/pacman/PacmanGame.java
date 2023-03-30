a665a6f18ff815ebeeaa24119cb508c1bfe56606
Daniil Lanin <d.lanin@g.nsu.ru>
2023-03-30 10:41:24 +0700

    add pacman in enemies list

--- HomeWork/Java/Pacman/src/main/java/ru/nsu/pacman/PacmanGame.java
+++ HomeWork/Java/Pacman/src/main/java/ru/nsu/pacman/PacmanGame.java
@@ -15,6 +15,10 @@ import javafx.util.Duration;
 import ru.nsu.pacman.enemy.Enemy;
 import ru.nsu.pacman.enemy.EnemyFactory;
 import ru.nsu.pacman.enemy.Pacman;
+import ru.nsu.pacman.enemy.ghosts.BlueGhost;
+import ru.nsu.pacman.enemy.ghosts.OrangeGhost;
+import ru.nsu.pacman.enemy.ghosts.PinkGhost;
+import ru.nsu.pacman.enemy.ghosts.RedGhost;
 import ru.nsu.pacman.generation.LevelBuilder;
 import ru.nsu.pacman.generation.LevelData;
 
@@ -44,28 +48,47 @@ public class PacmanGame extends Application {
     }
     
     public static class EnemyData {
-        public Enemy enemy;
-        public ImageView imageView = new ImageView();
-        
+        public Enemy body;
+        public ImageView view = new ImageView();
+
+        private Image passiveIMG;
+        private Image leftIMG;
+        private Image rightIMG;
+        private Image upIMG;
+        private Image downIMG;
+
         public EnemyData(Enemy enemy) {
-            this.enemy = enemy;
+            body = enemy;
 
-            this.imageView.setFitWidth(CELL_SIZE);
-            this.imageView.setFitHeight(CELL_SIZE);
+            view.setFitWidth(CELL_SIZE);
+            view.setFitHeight(CELL_SIZE);
 
-            this.imageView.setLayoutX(enemy.getPosition().x);
-            this.imageView.setLayoutY(enemy.getPosition().y);
+            view.setLayoutX(enemy.getPosition().x);
+            view.setLayoutY(enemy.getPosition().y);
+        }
+
+        public void setImages(Image passive, Image left, Image right, Image up, Image down) {
+            passiveIMG = passive;
+            leftIMG = left;
+            rightIMG = right;
+            upIMG = up;
+            downIMG = down;
+        }
+
+        public void changeOrientationVew() {
+            if (body.getCurrentOrientation() == Orientation.UP) {
+                view.setImage(upIMG);
+            } else if (body.getCurrentOrientation() == Orientation.LEFT) {
+                view.setImage(leftIMG);
+            } else if (body.getCurrentOrientation() == Orientation.RIGHT) {
+                view.setImage(rightIMG);
+            } else if (body.getCurrentOrientation() == Orientation.DOWN) {
+                view.setImage(downIMG);
+            } else {
+                view.setImage(passiveIMG);
+            }
         }
     }
-
-    //Images
-    private final Image pacmanRightIMG = new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/pacman/right.gif")));
-    private final Image pacmanLeftIMG = new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/pacman/left.gif")));
-    private final Image pacmanUpIMG = new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/pacman/up.gif")));
-    private final Image pacmanDownIMG = new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/pacman/down.gif")));
-    private final Image pacmanStoppedIMG = new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/pacman/stopped.png")));
-
-    private final Image redGhostIMG = new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif")));
     private GridPane area = null;
     LevelData data = null;
 
@@ -81,30 +104,79 @@ public class PacmanGame extends Application {
         launch(args);
     }
 
-    private ArrayList<Coordinates> getPositionsInData(LevelData data, LevelData.Symbols symbol) {
-        ArrayList<Coordinates> arr = new ArrayList<>();
-        for (int col = 0; col < CELL_N; ++col) {
-            for (int row = 0; row < CELL_N; ++row) {
-                if (data.getValueLevelData(new Coordinates(col, row)) == symbol) {
-                    arr.add(new Coordinates(col, row));
-                }
-            }
-        }
-        return arr;
-    }
     private ArrayList<EnemyData> getAllEnemies(LevelData data) throws InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
         ArrayList<EnemyData> result = new ArrayList<EnemyData>();
         for (int col = 0; col < CELL_N; ++col) {
             for (int row = 0; row < CELL_N; ++row) {
                 Enemy enemy = EnemyFactory.getInstance().createEnemy(data.getValueLevelData(new Coordinates(col, row)), new Coordinates(col, row), area, data);
-                if (enemy != null && enemy.getClass() != Pacman.class) {
+                if (enemy != null) {
                     result.add(new EnemyData(enemy));
                 }
             }
         }
         return result;
     }
+    private boolean settingIMG() {
+        for (EnemyData enemy : enemies) {
+            if (enemy.body.getClass().equals(Pacman.class)) {
+                enemy.setImages(new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/pacman/stopped.png"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/pacman/left.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/pacman/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/pacman/up.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/pacman/down.gif"))));
+            } else if (enemy.body.getClass().equals(RedGhost.class)) {
+                enemy.setImages(new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))));
+            } else if (enemy.body.getClass().equals(BlueGhost.class)) {
+                enemy.setImages(new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))));
+            } else if (enemy.body.getClass().equals(OrangeGhost.class)) {
+                enemy.setImages(new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))));
+            } else if (enemy.body.getClass().equals(PinkGhost.class)) {
+                enemy.setImages(new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))),
+                        new Image(Objects.requireNonNull(getClass().getResourceAsStream("sprites/ghosts/red/right.gif"))));
+            } else {
+                System.out.println("Enemy not found! Add new enemy in setting IMG");
+                return false;
+            }
+        }
+        return true;
+    }
 
+    private boolean addAllEnemiesInRoot(Pane root) {
+        boolean pacmanChecker = false;
+        for(int i = 0; i < enemies.size(); ++i) {
+            if (enemies.get(i).body.getClass() == Pacman.class) {
+                if (!pacmanChecker) {
+                    System.out.println("PACMAN ADDED!");
+                    pacman = enemies.get(i);
+                    pacmanChecker = true;
+                } else {
+                    System.out.println("In game play only 1 PACMAN!");
+                    return false;
+                }
+            }
+            root.getChildren().add(enemies.get(i).view);
+        }
+        if (!pacmanChecker) {
+            System.out.println("PACMAN NOT FOUND!");
+            return false;
+        }
+        return true;
+    }
     @Override
     public void start(Stage primaryStage) throws Exception {
         data = generateNextLevel();
@@ -114,30 +186,26 @@ public class PacmanGame extends Application {
         Scene scene = new Scene(root);
         scene.setFill(Color.AQUA);
 
-        if (getPositionsInData(data, LevelData.Symbols.Pacman).size() != 1) {
-            throw new Exception("There can only be one pacman in the game");
-        }
-        pacman = new EnemyData(new Pacman(getPositionsInData(data, LevelData.Symbols.Pacman).get(0), area, data));
-        pacman.imageView.setImage(pacmanStoppedIMG);
         enemies = getAllEnemies(data);
 
-        root.getChildren().add(pacman.imageView);
-        for(int i = 0; i < enemies.size(); ++i) {
-            root.getChildren().add(enemies.get(i).imageView);
+        if (!settingIMG()) {
+            return;
+        }
+        if (!addAllEnemiesInRoot(root)) {
+            return;
         }
 
         scene.setOnKeyPressed(event -> {
             if (event.getCode() == KeyCode.UP) {
-                pacman.enemy.changeNextOrientation(Orientation.UP);
+                pacman.body.changeNextOrientation(Orientation.UP);
             } else if (event.getCode() == KeyCode.DOWN) {
-                pacman.enemy.changeNextOrientation(Orientation.DOWN);
+                pacman.body.changeNextOrientation(Orientation.DOWN);
             } else if (event.getCode() == KeyCode.LEFT) {
-                pacman.enemy.changeNextOrientation(Orientation.LEFT);
+                pacman.body.changeNextOrientation(Orientation.LEFT);
             } else if (event.getCode() == KeyCode.RIGHT) {
-                pacman.enemy.changeNextOrientation(Orientation.RIGHT);
+                pacman.body.changeNextOrientation(Orientation.RIGHT);
             } else if (event.getCode() == KeyCode.ESCAPE) {
                 pause = !pause;
-                pacman.imageView.setImage(pacmanStoppedIMG);
             }
         });
 
@@ -157,54 +225,31 @@ public class PacmanGame extends Application {
 
     private void update() {
         if (inGame && !pause) {
-            //PacmanAnimation
-            pacman.enemy.move();
+            //GhostsAnimation
+            for (int i = 0; i < enemies.size(); ++i) {
+                EnemyData enemy = enemies.get(i);
 
-            pacman.imageView.setLayoutX(pacman.enemy.getPosition().x);
-            pacman.imageView.setLayoutY(pacman.enemy.getPosition().y);
+                enemy.body.move();
 
-            if (pacman.enemy.getCurrentOrientation() == Orientation.UP) {
-                pacman.imageView.setImage(pacmanUpIMG);
-            } else if (pacman.enemy.getCurrentOrientation() == Orientation.LEFT) {
-                pacman.imageView.setImage(pacmanLeftIMG);
-            } else if (pacman.enemy.getCurrentOrientation() == Orientation.RIGHT) {
-                pacman.imageView.setImage(pacmanRightIMG);
-            } else if (pacman.enemy.getCurrentOrientation() == Orientation.DOWN) {
-                pacman.imageView.setImage(pacmanDownIMG);
-            } else {
-                pacman.imageView.setImage(pacmanStoppedIMG);
+                enemy.view.setLayoutX(enemy.body.getPosition().x);
+                enemy.view.setLayoutY(enemy.body.getPosition().y);
+
+                enemy.changeOrientationVew();
+
+                //Check mob collision
+                if (enemy.body.getClass() != Pacman.class) {
+                    if (getDistance(pacman.body, enemy.body) <= CELL_SIZE * 0.8) {
+                        System.out.println("YOU LOSE!");
+                        inGame = false;
+                        break;
+                    }
+                }
             }
 
+            //Check finish
             if (data.getEatedFood() == data.getCountFood()) {
                 inGame = false;
                 System.out.println("YOU WIN!");
-            }
-
-            //GhostsAnimation
-            for (int i = 0; i < enemies.size(); ++i) {
-                EnemyData ghost = enemies.get(i);
-
-                ghost.enemy.move();
-
-                ghost.imageView.setLayoutX(ghost.enemy.getPosition().x);
-                ghost.imageView.setLayoutY(ghost.enemy.getPosition().y);
-
-                if (ghost.enemy.getCurrentOrientation() == Orientation.UP) {
-                    ghost.imageView.setImage(redGhostIMG);
-                } else if (ghost.enemy.getCurrentOrientation() == Orientation.LEFT) {
-                    ghost.imageView.setImage(redGhostIMG);
-                } else if (ghost.enemy.getCurrentOrientation() == Orientation.RIGHT) {
-                    ghost.imageView.setImage(redGhostIMG);
-                } else if (ghost.enemy.getCurrentOrientation() == Orientation.DOWN) {
-                    ghost.imageView.setImage(redGhostIMG);
-                }
-
-                //Check mob collision
-                if (getDistance(pacman.enemy, ghost.enemy) <= CELL_SIZE * 0.8) {
-                    System.out.println("YOU LOSE!");
-                    inGame = false;
-                    break;
-                }
             }
         }
     }
